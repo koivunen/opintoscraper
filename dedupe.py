2797082ed4603e97d1e5c527070b744a20536984
lamkoi <lamkoi@utu.fi>
2022-01-24 18:05:06 +0200

    gitignore+deduplicator WIP

---
+++ dedupe.py
@@ -0,0 +1,43 @@
+
+from constants import *
+import code
+from copy import deepcopy
+import pathlib
+from pathlib import Path
+import logging
+import hashlib
+
+
+
+def dedupeSubfolders(path):
+    global_hashes=[]
+    for folder in path.iterdir():
+        if not folder.is_dir():
+            continue
+        #print("checking",folder)
+        hashes=[]
+        for file in folder.iterdir():
+            if file.is_dir():
+                continue
+            #print("\t",file)
+            sha1 = hashlib.sha1()
+            with file.open("rb") as f:
+                sha1.update(f.read())
+
+            digest=sha1.digest()
+            if digest in hashes:
+                print("DEL",file)
+                file.unlink()
+            else:
+                hashes.append(digest)
+
+        for h in hashes:
+            if h in global_hashes:
+                print("wtf?",folder)
+            else:
+                global_hashes.append(h)
+
+if __name__ == '__main__':
+    output_path = Path(OUTPUT_PATH)
+    assert output_path.exists()
+    dedupeSubfolders(output_path)
