156dea123b8aa5de4e468294343cd3ecf124751d
lamkoi <lamkoi@utu.fi>
2022-01-26 10:54:42 +0200

    Add readme and use database for application dupes

--- README.md
+++ README.md
@@ -1,15 +1,49 @@
 # opintoscraper
 
-Attachments scraping for https://virkailija.opintopolku.fi/lomake-editori/
+A web scraper for the attachments from https://virkailija.opintopolku.fi/lomake-editori/ for mass processing of up to thousands of applications. This process is not officially supported and may break at any moment, you have been warned.
 
-### Likely the correct documentation path for a PROPER API
+### Requirements
 
-https://wiki.eduuni.fi/display/ophpolku/Hakeneet+ja+paikan+vastaanottaneet+%28kk%29+v4
+ - Microsoft edge or a browser
+ - Python 3.9
+ - Programming experience (for now)
+
+### Usage
+
+ - Install dependencies from requirements.txt (`pip instal -r requirements.txt`)
+ - Set up target program IDs in `constants.py` (program groups are not supported yet!)
+ - Login to [https://virkailija.opintopolku.fi/](https://virkailija.opintopolku.fi/) in Microsoft Edge.
+ - Run `main.py` and watch for the process to fail or finish. 
+    - You may be logged out of opintopolku in the middle in which case you need to login back with edge and press enter. The program will prompt you for this.
+   - On failure: you may need to rerun the program in which case it skips every application already dowloaded based on the `database` file next to main.py
+ - After mass download, run `dedupe.py` to remove duplicate application files with different filenames and 
+ - Rename/remove `database*` files to reinitialize the downloader
+
+
+### Default output format description
+
+  - The attachments will be saved into `output` folder with original filenames.
+     - Duplicate names are prefixed with _n0 _n1 and so forth
+  - The folder format is `last name_Preferred first_personOID` so for example `meikäläinen_matti_1.2.3.123124345235235`.
+     - NOTE: A person may have multiple applications with different names so there may exist for example.
+     - IN ADDITION: Multiple applications with attachments may get merged to the same folder
+
+### TODO / issues / Help wanted
+
+ - No GUI
+ - no easy way to use/configure 
+ - Does not use official APIs.
+ - not configurable output target format (for example per-program folder for attachments)
+
+
+## Misc
+Likely the correct documentation path for a PROPER API: https://wiki.eduuni.fi/display/ophpolku/Hakeneet+ja+paikan+vastaanottaneet+%28kk%29+v4
 
 Also check (api complains if you don't give caller-id):
 https://github.com/Opetushallitus/dokumentaatio/blob/master/http.md
 
-### Misc links
+Misc links:
+
 https://wiki.eduuni.fi/display/ophoppija/Hakemuspalvelun+REST+API
 
 https://virkailija.opintopolku.fi/suoritusrekisteri/swagger/index.html#/kkhakijat/haeKkHakijat
--- main.py
+++ main.py
@@ -13,35 +13,17 @@ output_path = Path(OUTPUT_PATH)
 output_path.mkdir(exist_ok=True)
 import cgi
 import shelve
-db=shelve.open('database')
 from concurrent import futures
-
-
+import filecmp
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
-
 retries = Retry(total=5, backoff_factor=1)
-
-
-# OLD Folder path plan:
-# output/matti_meikalainen/kokkikoulu/hygienia.pdf
-# output/matti_meikalainen/kokkikoulu/passi.pdf
-# output/matti_meikalainen/koodikoulu/atkajokortti.pdf
-# output/matti_meikalainen/koodikoulu/passi.pdf
-# output/matti_meikalainen/oid.lnk ?
-# new
-# output/matti/passi.pdf
-
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
 
 import http.client as http_client
 if DEBUG:
     http_client.HTTPConnection.debuglevel = 1
 
-# You must initialize logging, otherwise you'll not see debug output.
 logging.basicConfig()
 if DEBUG:
     logging.getLogger().setLevel(logging.DEBUG)
@@ -50,16 +32,32 @@ if DEBUG:
     requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
+## Application database
+
+db = shelve.open('database')
+
+
+def hasDownloadedApplication(oid):
+    return db.get(oid)
+
+
+def markAsDownloaded(oid):
+    db[oid] = True
+
+
 # Common session used to store cookies, also with retry
 session = requests.Session()
 session.mount('http://', HTTPAdapter(max_retries=retries))
 session.mount('https://', HTTPAdapter(max_retries=retries))
 
+
 def rebuildCookies():
     cookies = browser_cookie3.edge()  #browser_cookies = browser_cookie3.load()
     session.cookies = cookies
 
+
 rebuildCookies()
+
 
 def getCSRF():
     return session.cookies._cookies[".opintopolku.fi"]["/"]["CSRF"].value
@@ -73,15 +71,20 @@ def generateHeader():
         "caller-id": "requestscraper-utu-tech-lamkoiatutufi"
     }
 
+
 def testLoggedIn():
     # Test if logged in
     try:
         infotest = session.get(ME2_INFO_URL)
     except:
         return False
-    return infotest.status_code==200
+    return infotest.status_code == 200
+
+
 while not testLoggedIn():
-    input("Could not login to https://virkailija.opintopolku.fi/ . Try logging in with a browser (microsoft edge) and press enter.")
+    input(
+        "Could not login to https://virkailija.opintopolku.fi/ . Try logging in with a browser (microsoft edge) and press enter."
+    )
     rebuildCookies()
 
 
@@ -104,38 +107,53 @@ def downloadFileTo(file_guid, target_folder):
     assert attachment_file.status_code == 200
     filename = cgi.parse_header(
         attachment_file.headers["Content-Disposition"])[1]["filename"]
-    path=(target_folder / filename)
-    
+    path = (target_folder / filename)
+    opath=path
     if path.exists():
-        opath=path
         for i in range(99999):
-            path=opath.with_stem(opath.stem+"_n"+str(i))
+            path = opath.with_stem(opath.stem + "_n" + str(i))
             if not path.exists():
                 break
 
     with path.open("wb") as f:
         for chunk in attachment_file.iter_content(chunk_size=8192):
             f.write(chunk)
+    if path!=opath and filecmp.cmp(path,opath):
+        path.unlink()
+        print("\t\t","^ is dupe to",opath,"removing")
+        path=opath
+        #TODO: compare all "revision"
+
     return path
 
-oid_processed={}
+
+oid_processed = {}
+
+
 def processApplication(application):
-    
+
     #oid=application["person"]["oid"]
     oid = application["key"]
+    application_oid = oid
     if oid_processed.get(oid):
-        print("duplicate",oid)
+        print("Already processed", oid, "(this session)")
         return
 
-    oid_processed[oid]=True
-    
-    folder_name = "{0}_{1}_{2}".format(application["person"]["last-name"],application["person"]["preferred-name"],application["person"]["oid"])
+    oid_processed[oid] = True
+
+    folder_name = "{0}_{1}_{2}".format(application["person"]["last-name"],
+                                       application["person"]["preferred-name"],
+                                       application["person"]["oid"])
     person_path = output_path / folder_name
-    if person_path.exists():
-        print("\nSkip",folder_name)
+
+    # UNDONE: DANGEROUS: Would not download attachments for people with multiple applications
+    #if person_path.exists():
+
+    if hasDownloadedApplication(oid):
+        print("\nAlready processed", folder_name)
         return
     else:
-        print("\nProcessing",folder_name)
+        print("\nProcessing", folder_name)
     person_path.mkdir(exist_ok=True)
 
     #with (person_path/"info.txt").open("w") as f:
@@ -144,8 +162,8 @@ def processApplication(application):
     #    f.write(application["person"]["preferred-name"])
 
     target_path = (person_path)
-    Path(target_path).mkdir(exist_ok=True,parents=True)
-    
+    Path(target_path).mkdir(exist_ok=True, parents=True)
+
     # details not in list, such as attachment info
     url = APPLICATION_URL.format(oid=oid)
     application_details = session.get(url, headers=generateHeader())
@@ -158,35 +176,38 @@ def processApplication(application):
     assert application_details.status_code == 200
 
     application_details = application_details.json()
-    filtered_attachments=[]
+    filtered_attachments = []
     for oid, val in application_details['attachment-reviews'].items():
         if oid in TARGET_OIDS:
-            filtered_attachments+=list(val.keys())
+            filtered_attachments += list(val.keys())
 
     for answer in application_details["application"]["answers"]:
         if answer["fieldType"] != "attachment":
             continue
         key = answer["key"]
         if key not in filtered_attachments:
-            print("\t","filtered",key,"for",person_path)
+            print("\t", "filtered", key, "for", person_path)
             continue
 
         values = answer["value"]
         # sometimes it's a list of strings, sometimes a list of lists
-        downloadables=[]
+        downloadables = []
         for attachment_file_guids in values or []:
 
             # sometimes it's a list, sometimes a string (but why?)
             if isinstance(attachment_file_guids, str):
-                attachment_file_guids=[attachment_file_guids]
+                attachment_file_guids = [attachment_file_guids]
             for attachment_file_guid in attachment_file_guids or []:
 
                 downloadables.append(attachment_file_guid)
-                
+
         with futures.ThreadPoolExecutor(max_workers=5) as executor:
-            res = executor.map(lambda x: downloadFileTo(x,target_path),downloadables)
+            res = executor.map(lambda x: downloadFileTo(x, target_path),
+                               downloadables)
             for r in res:
-                print("\tDownloaded",r)
+                print("\tDownloaded", r)
+    markAsDownloaded(application_oid)
+
 
 # Iterate over application targets (?)
 for oid, oid_folder_name in TARGET_OIDS.items():
@@ -194,7 +215,9 @@ for oid, oid_folder_name in TARGET_OIDS.items():
     query = deepcopy(LIST_URL_QUERY)
     query["hakukohde-oid"] = oid
 
-    applications_list = session.post(LIST_URL, json=query, headers=generateHeader())
+    applications_list = session.post(LIST_URL,
+                                     json=query,
+                                     headers=generateHeader())
 
     assert applications_list.status_code == 200
 
@@ -205,6 +228,5 @@ for oid, oid_folder_name in TARGET_OIDS.items():
           len(applications_list["applications"]))
     for application in applications_list["applications"]:
         processApplication(application)
-        
 
-    
+print("Processing finished!")
